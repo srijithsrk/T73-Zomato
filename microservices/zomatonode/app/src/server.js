f2ec75f01765139dcc3985a054c2df4432da24e5
Soumyadip <sharmasoumyadip@gmail.com>
2018-01-10 02:12:31 +0530

    port changed

--- microservices/zomatonode/app/src/server.js
+++ microservices/zomatonode/app/src/server.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
     res.send("Hello World!");
 });
 
-app.listen(8080, function () {
+app.listen(8000, function () {
   console.log('Example app listening on port 8080!');
 });
